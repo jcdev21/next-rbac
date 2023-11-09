8c1f67a33b1954004ce3c4f18b67004a6c44592c
jcdev21 <jcdev21@gmail.com>
2023-11-09 13:02:00 +0700

    remove migration

--- prisma/migrations/20231103032708_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[menuId]` on the table `accesses` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `accesses_menuId_key` ON `accesses`(`menuId`);
